c572fd660dfe5e72c165bed865dc6c274d997f3f
CuebongW <cuebong@hotmail.com>
2014-10-13 17:46:03 +0800

    Improved code for easier use

---
+++ python/variance_ellipse.py
@@ -0,0 +1,39 @@
+from numpy import *
+from matplotlib.patches import Ellipse
+from pylab import *
+
+def variance_ellipse(sample,x1,x2,x3,y1,y2,y3,i):
+    variability=[]
+
+    for j in xrange(0,len(sample)):
+        data=array([[x1[j],y1[j]],[x2[j],y2[j]],[x3[j],y3[j]]])
+        data2=array(data)
+
+        meanx=mean(data[:,0])
+        meany=mean(data[:,1])
+    
+        for l in range(data.shape[0]):
+            data2[l,0]=data[l,0]-meanx
+            data2[l,1]=data[l,1]-meany
+
+        ax=gca()
+
+        xy=(meanx,meany)
+        u,d,v=linalg.svd(data2)
+
+        width=d[0]
+        height=d[1]
+
+        angle=arctan2(v[0,1],v[0,0])*180/pi
+        variability.append(sqrt(width*width+height*height))
+
+        if i==0:
+            edgecolour='c'
+        elif i==1:
+            edgecolour='y'
+        else:
+            edgecolour='m'
+        ellipse=Ellipse(xy,width,height,angle,edgecolor=edgecolour,fc='none',lw=1)
+        ax.add_patch(ellipse)
+
+    return variability
