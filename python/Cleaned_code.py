0b14f2d2dbee663a01a2a1dc0412ab0deeb612c8
CuebongW <cuebong@hotmail.com>
2014-10-20 17:54:50 +0800

    Trajectory cutting code added

--- python/Cleaned_code.py
+++ python/Cleaned_code.py
@@ -15,31 +15,18 @@ from read_filenames import get_filepaths
 import sys
 from mean_traj import mean_calc
 from variance_ellipse import variance_ellipse
-from scipy.signal import freqz
-from scipy.signal import butter, lfilter
+from butter_filter import butter_lowpass, butter_lowpass_filter
 
-def butter_lowpass(lowcut, fs, order=2):
-    nyq = 0.5 * fs
-    low = lowcut / nyq
-    b, a = butter(order, low)
-    return b, a
-
-def butter_lowpass_filter(data, lowcut, fs, order=2):
-    b, a = butter_lowpass(lowcut, fs, order=order)
-    y = lfilter(b, a, data)
-    return y
-
-fs=2500
+fs=3500
 lowcut=6.25
 
-"""
+"""#---Displaying butterworth filter---
 plt.figure(5)
 plt.clf()
 b,a=butter_lowpass(lowcut,fs,order=2)
 w,h=freqz(b,a,worN=500)
 plt.plot((fs*0.5/np.pi)*w,abs(h))
 """
-
 
 plt.ion() #turns on interactive mode
 fig1=plt.figure(1)
@@ -50,7 +37,7 @@ file_paths.append(get_filepaths("/home/cuebong/git/cri4/data_8_10_14/P04"))
 file_paths.append(get_filepaths("/home/cuebong/git/cri4/data_8_10_14/P04"))
 use_files,nf,meanX,meanY,sample,Xax,Yax=[],[],[],[],[],[],[]
 MPX,MPY,MPZ,time,x,y,xvalues,yvalues=[],[],[],[],[],[],[],[]
-fX,fY=[],[]
+fX,fY,time_vel,vel_x,vel_y,vel=[],[],[],[],[],[]
 
 for i in xrange(0,len(file_paths)):
     use_files.append([])
@@ -80,6 +67,10 @@ for i in xrange(0,len(file_paths)):
     Yax.append([])
     fX.append([])
     fY.append([])
+    time_vel.append([])
+    vel_x.append([])
+    vel_y.append([])
+    vel.append([])
 
     for j in xrange(0,len(use_files[i])):
         with open(use_files[i][j]) as f:
@@ -101,6 +92,67 @@ for i in xrange(0,len(file_paths)):
 	    xvalues[i].append(g)
 	    yvalues[i].append(h)
 
+plt.close(1)
+
+#__________Velocity_Profile_______________________
+def resultant_profile(x,y):
+    val=[]
+    for i in xrange(0,len(x)):
+        x_term=x[i]*x[i]
+        y_term=y[i]*y[i]
+        a=(x_term+y_term)**0.5
+        val.append(a)
+    return val
+
+for i in xrange(0,len(file_paths)):
+    for j in xrange(0,len(use_files[i])):
+        a,b=deriv_vel(x[i][j])
+        time_vel[i].append(a)
+        vel_x[i].append(b)
+        a,c=deriv_vel(y[i][j])
+        vel_y[i].append(c)
+        d=resultant_profile(vel_x[i][j],vel_y[i][j])
+        vel[i].append(d)
+
+        while len(time_vel[i][j])!=len(vel[i][j]):
+            if len(time_vel[i][j])>len(vel[i][j]):
+                time_vel[i][j]=np.delete(time_vel[i][j],(len(time_vel[i][j])-1))
+            else:
+                vel[i][j]=np.delete(vel[i][j],len(vel[i][j])-1)
+                print(len(vel[i][j]))
+
+        for l in xrange(0, len(time_vel[i][j])):
+            m=len(time_vel[i][j])-l-1
+            n=vel[i][j][m]
+            if n>0.2:
+                break
+        vel[i][j]=np.delete(vel[i][j],xrange(m,len(vel[i][j])))
+        time_vel[i][j]=np.delete(time_vel[i][j],xrange(m,len(time_vel[i][j])))
+        x[i][j]=np.delete(x[i][j],xrange(m,len(x[i][j])))
+        y[i][j]=np.delete(y[i][j],xrange(m,len(y[i][j])))
+        fX[i][j]=np.delete(fX[i][j],xrange(m,len(fX[i][j])))
+        fY[i][j]=np.delete(fY[i][j],xrange(m,len(fY[i][j])))
+
+        for l in xrange(0, len(time_vel[i][j])):
+            n=vel[i][j][l]
+            if n>0.2:
+                break
+        vel[i][j]=np.delete(vel[i][j],xrange(0,l))
+        time_vel[i][j]=np.delete(time_vel[i][j],xrange(0,l))
+        x[i][j]=np.delete(x[i][j],xrange(0,l))
+        y[i][j]=np.delete(y[i][j],xrange(0,l))
+        xvalues[i][j]=np.delete(xvalues[i][j],xrange(0,l))
+        yvalues[i][j]=np.delete(yvalues[i][j],xrange(0,l))
+        fX[i][j]=np.delete(fX[i][j],xrange(0,l))
+        fY[i][j]=np.delete(fY[i][j],xrange(0,l))
+
+    #plt.figure()
+    #plt.plot(time_vel[i][0],vel[i][0],time_vel[i][1],vel[i][1],time_vel[i][2],vel[i][2])
+
+#__________Mean_Trajectory________________________
+
+for i in xrange(0,len(file_paths)):
+
     nf.append(compare(time[i][0],time[i][1],time[i][2]))
 
     a,b,c,d,e,f,g,h,k=mean_calc(nf[i],x[i][0],x[i][1],x[i][2],y[i][0],y[i][1],y[i][2],xvalues[i][0],xvalues[i][1],xvalues[i][2],yvalues[i][0],yvalues[i][1],yvalues[i][2])
@@ -114,8 +166,7 @@ for i in xrange(0,len(file_paths)):
     Yax[i].append(h)
     Yax[i].append(k)
 
-plt.close(1)
-"""
+"""#Displaying original (unfiltered) trjectory paths
 fig2=plt.figure(2)
 plt.plot(meanX[0],meanY[0],meanX[1],meanY[1],meanX[2],meanY[2])
 traj1=plt.plot(MPX[0][0],MPY[0][0],MPX[0][1],MPY[0][1],MPX[0][2],MPY[0][2])
@@ -130,15 +181,14 @@ plt.ylabel=('Y-axis')
 fig2.suptitle('Mean Trajectory')
 """
 
-#_____________________________________________________________________
-
-fig3=plt.figure(3)
+#__________Variance_Ellipses______________________
+fig4=plt.figure(4)
 plt.xlim(0, 5)
 plt.ylim(0, 5)
 plt.gca().set_aspect('equal', adjustable='box')
 plt.xlabel('X-axis')
 plt.ylabel('Y-axis')
-fig3.suptitle('P04 Trajectories - Visual Forward')
+fig4.suptitle('P04 Trajectories - Visual Forward')
 
 variability=[]
 
@@ -160,7 +210,7 @@ params = {'legend.fontsize': 8,
 plt.rcParams.update(params)
 plt.legend(loc='upper right')
 
-fig4=plt.figure(4)
+fig5=plt.figure(5)
 plt.plot(sample[0],variability[0],label='C2N_S_vert_VF')
 plt.plot(sample[1],variability[1],label='C4E_L_bleu_VF')
 plt.plot(sample[2],variability[2],label='C4W_R_rouge_VF')
@@ -168,18 +218,9 @@ plt.xlim(0, 1)
 plt.ylim(0, 1.9)
 plt.xlabel('Time')
 plt.ylabel('Variability')
-fig4.suptitle('P04 Variance Profile - Visual Forward')
+fig5.suptitle('P04 Variance Profile - Visual Forward')
 params = {'legend.fontsize': 8,
           'legend.linewidth': 2}
 plt.rcParams.update(params)
 plt.legend(loc='upper left')
 
-#_____________________________________________________#
-"""
-for i in xrange(0,len(MPX[2][1])):
-    x.append(float(MPX[2][1][i]))
-    y.append(float(MPY[2][1][i]))
-
-newx=butter_lowpass_filter(x,lowcut,fs,order=2)
-newy=butter_lowpass_filter(y,lowcut,fs,order=2)
-"""
